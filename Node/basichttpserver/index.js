d0802ba8810d93e0a21af2418c4d347c2a9b83b4
deepzsenu <deepzsenu@gmail.com>
2023-06-24 14:17:36 +0530

    basic server created and rendered html

---
+++ Node/basichttpserver/index.js
@@ -0,0 +1,57 @@
+var http = require("http");
+
+var port = 8080;
+
+const fs = require("fs");
+
+function requestHandler(req,res){
+    console.log(req.url);
+    res.writeHead(200, {"Content-Type": "text/html"});
+
+    let filePath ;
+
+    switch(req.url){
+        case '/':
+            filePath = "./index.html";
+            break;
+        case '/profile':
+            filePath = "./profile.html";
+            break;
+        default:
+            filePath = "./404.html";
+    }
+
+
+    fs.readFile(filePath, function(err, data){
+        if(err){
+            console.log(err);
+            return res.end("<h1>Error<h1>")
+        }
+
+        return res.end(data);
+    })
+
+    // fs.readFile("./index.html", function(err,data){
+    //     if(err){
+    //         console.log("Error " , err);
+    //         return res.end("<h1>Error found<h1>");
+    //     }
+
+    //     return res.end(data);
+    // });
+
+    // res.end("<h1> Server me!<h1>");
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    else{
+        console.log("server is up at :", port);
+        return
+    }
+})
